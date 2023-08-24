0cb02ca8a907522e3f67034fbf857d6da3cc8f0a
Luis Gustavo Furtuozo Falci <luisgustavo.1999@hotmail.com>
2023-08-24 19:34:50 -0300

    criar e editar mais ou menos

--- 16_ROOM/app/src/main/java/com/example/a16_room/ui/view/EditStudentActivity.kt
+++ 16_ROOM/app/src/main/java/com/example/a16_room/ui/view/EditStudentActivity.kt
@@ -2,11 +2,45 @@ package com.example.a16_room.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.a16_room.R
+import com.example.a16_room.databinding.ActivityMainBinding
+import com.example.a16_room.ui.viewmodels.MainViewModel
 
 class EditStudentActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
+    private var studentId: Int = -1
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_student)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Toast.makeText(applicationContext, "edit", Toast.LENGTH_SHORT).show()
+        // Initialize your viewModel here
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        if (intent.hasExtra("student_id")) {
+            studentId = intent.getIntExtra("student_id", -1)
+        }
+        if (studentId > 0) {
+            viewModel.get(studentId)
+            viewModel.student.observe(this) { student ->
+                studentId = student.id
+                binding.editName.setText(student.name)
+                binding.editRegistration.setText(student.registration)
+            }
+        }
+
+        binding.buttonEdit.setOnClickListener {
+            val name = binding.editName.text.toString()
+            val registration = binding.editRegistration.text.toString()
+            viewModel.update(studentId, name, registration)
+
+            // Optionally, you can also finish the activity after updating
+            finish()
+        }
+
     }
 }
--- 16_ROOM/app/src/main/java/com/example/a16_room/ui/view/MainActivity.kt
+++ 16_ROOM/app/src/main/java/com/example/a16_room/ui/view/MainActivity.kt
@@ -1,72 +1,76 @@
-    package com.example.a16_room.ui.view
+package com.example.a16_room.ui.view
 
-    import androidx.appcompat.app.AppCompatActivity
-    import android.os.Bundle
-    import android.widget.Toast
-    import androidx.lifecycle.ViewModelProvider
-    import androidx.recyclerview.widget.LinearLayoutManager
-    import com.example.a16_room.ui.adapters.StudentAdapter
-    import com.example.a16_room.ui.listeners.OnStudentListener
-    import com.example.a16_room.ui.viewmodels.MainViewModel
-    import com.example.a16_room.databinding.ActivityMainBinding
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.a16_room.ui.adapters.StudentAdapter
+import com.example.a16_room.ui.listeners.OnStudentListener
+import com.example.a16_room.ui.viewmodels.MainViewModel
+import com.example.a16_room.databinding.ActivityMainBinding
 
-    class MainActivity : AppCompatActivity() {
-        private lateinit var binding: ActivityMainBinding
-        private lateinit var viewModel: MainViewModel
-        private val adapter = StudentAdapter()
-        private var id = 0
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    private val adapter = StudentAdapter()
+    private var id = 0
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            super.onCreate(savedInstanceState)
-            setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
 
-            viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
-            binding.recyclerStudents.layoutManager = LinearLayoutManager(applicationContext)
-            binding.recyclerStudents.adapter = adapter
+        binding.recyclerStudents.layoutManager = LinearLayoutManager(applicationContext)
+        binding.recyclerStudents.adapter = adapter
 
-            val listener = object : OnStudentListener {
-                override fun OnClick(id: Int) {
-                    Toast.makeText(applicationContext, id.toString(), Toast.LENGTH_SHORT).show()
-                    viewModel.get(id)
-                }
+        val Intent = Intent(this, EditStudentActivity::class.java)
+        val listener = object : OnStudentListener {
+            override fun OnClick(id: Int) {
+                Intent.putExtra("student_id", id)
+                startActivity(Intent)
+//                Toast.makeText(applicationContext, id.toString(), Toast.LENGTH_SHORT).show()
+//                viewModel.get(id)
             }
+        }
 
-            adapter.attachListener(listener)
+        adapter.attachListener(listener)
 
-            binding.buttonInsert.setOnClickListener {
-                val name = binding.editName.text.toString()
-                val registration = binding.editRegistration.text.toString()
+        binding.buttonInsert.setOnClickListener {
+            val name = binding.editName.text.toString()
+            val registration = binding.editRegistration.text.toString()
 
-                viewModel.insert(name, registration)
+            viewModel.insert(name, registration)
 
-            }
-            binding.buttonEdit.setOnClickListener {
-                val name = binding.editName.text.toString()
-                val registration = binding.editRegistration.text.toString()
-                viewModel.update(id, name, registration)
-            }
-            binding.buttonDelete.setOnClickListener {
-                viewModel.delete(id)
-            }
+        }
+        binding.buttonEdit.setOnClickListener {
+            val name = binding.editName.text.toString()
+            val registration = binding.editRegistration.text.toString()
+            viewModel.update(id, name, registration)
+        }
+        binding.buttonDelete.setOnClickListener {
+            viewModel.delete(id)
+        }
 
-            observe()
+        observe()
+        viewModel.getAll()
+    }
+
+    private fun observe() {
+        viewModel.students.observe(this) {
+            adapter.updateStudents(it)
+        }
+        viewModel.student.observe(this) {
+            id = it.id
+            binding.textId.setText(id.toString())
+            binding.editName.setText(it.name)
+            binding.editRegistration.setText(it.registration)
+        }
+        viewModel.newChange.observe(this) {
             viewModel.getAll()
         }
-
-        private fun observe() {
-            viewModel.students.observe(this) {
-                adapter.updateStudents(it)
-            }
-            viewModel.student.observe(this) {
-                id = it.id
-                binding.textId.setText(id.toString())
-                binding.editName.setText(it.name)
-                binding.editRegistration.setText(it.registration)
-            }
-            viewModel.newChange.observe(this) {
-                viewModel.getAll()
-            }
-        }
     }
+}
